adae284129a3d8f4a722b18a2ae7e9d03443f408
Alexander Tsuji <asooge@gmail.com>
2022-11-16 21:06:55 -0400

    home page customize and refactor

---
+++ components/Footer.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+export const Footer: React.FC = () => {
+  return (
+    <footer className={styles.footer}>
+      <a
+        href="https://github.com/asooge"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Developed by asooge
+        <span>
+          <Image
+            src="/asooge-profile.jpeg"
+            alt="Vercel Logo"
+            width={24}
+            height={24}
+            style={{ borderRadius: "50%", marginLeft: "4px" }}
+          />
+        </span>
+      </a>
+    </footer>
+  );
+};
---
+++ components/Head.tsx
@@ -0,0 +1,11 @@
+import NextHead from "next/head";
+
+export const Head: React.FC = () => {
+  return (
+    <NextHead>
+      <title>TypeStorm</title>
+      <meta name="description" content="See how fast you can type" />
+      <link rel="icon" href="/favicon.ico" />
+    </NextHead>
+  );
+};
